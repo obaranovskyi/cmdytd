94bc1e0682219f6b3e227ad271cf855df78acdd1
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-12-16 08:31:21 +0200

    Added downlaod channel feature

--- cmdytd/channel/video/subparser.py
+++
@@ -1,8 +0,0 @@
-from .handler import handle
-from ..shared.parser import subparsers
-
-
-def video_subparser():
-    video = subparsers.add_parser('video', help='Download video file from YouTube.')
-    video.add_argument('video_id', type=str, help='Id of video that has to be downloaded.')
-    video.set_defaults(func=handle)
